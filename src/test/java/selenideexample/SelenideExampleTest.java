cdab1ae0bfa7033bceeed8202873435eab8a9138
Mykola <mbskyba@gmail.com>
2022-08-10 13:29:50 +0300

    allure_commit

--- src/test/java/selenideexample/SelenideExampleTest.java
+++ src/test/java/selenideexample/SelenideExampleTest.java
@@ -1,32 +1,50 @@
 package selenideexample;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SelenideExampleTest {
 
+
     @Test
-    public void selenideTest() {
+    @Step(value = "Logging In")
+    public void loggingIn() {
+
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
-        open("https://google.com");
-        $("[name='q']")
-                .shouldBe(visible)
-                .setValue("Selenide")
-                .pressEnter();
-        $$x("//h3[contains(text(), 'Selenide')]")
-                .filter(visible)
-                .shouldHave(sizeGreaterThanOrEqual(7))
-                .get(1)
+        open("http://the-internet.herokuapp.com/login");
+        $x("//*[@id=\"username\"]")
+                .setValue("tomsmith");
+        $x("//*[@id=\"password\"]")
+                .setValue("SuperSecretPassword!");
+        $x("//i[contains(text(),'Login')]")
                 .click();
-        $(".donate_header")
-                .shouldHave(text("Selenide Supports Ukraine \uD83C\uDDFA\uD83C\uDDE6"));
+        $x("//*[@id=\"flash\"]")
+                .isDisplayed();
+
+
+    }
+
+    @Test
+    @Step(value = "Logging Out")
+    public void loggingOut() {
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+        open("http://the-internet.herokuapp.com/secure");
+        $x("//i[contains(text(),'You logged into a secure area!')]")
+                .shouldNotBe(Condition.visible);
+        $x("//a[@class='button secondary radius']")
+                .click();
+
+
     }
 
 }
